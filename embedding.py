bea7ec3dd7b946a3328371d7cabb4e6076631c3f
ziaoang <ziaoang@gmail.com>
2016-05-20 19:04:43 +0900

    small change

--- embedding.py
+++ embedding.py
@@ -29,38 +29,29 @@ user_count = 6040 + 1
 item_count = 3952 + 1
 
 model_left = Sequential()
-model_left.add(Embedding(user_count, 10, input_length=1))
+model_left.add(Embedding(user_count, 20, input_length=1))
 model_right = Sequential()
-model_right.add(Embedding(item_count, 10, input_length=1))
+model_right.add(Embedding(item_count, 20, input_length=1))
 model = Sequential()
 model.add(Merge([model_left, model_right], mode='concat'))
 model.add(Flatten())
-model.add(Dense(64))
+model.add(Dense(20))
 model.add(Activation('sigmoid'))
-model.add(Dense(64))
+model.add(Dense(10))
 model.add(Activation('sigmoid'))
 # model.add(Dense(64))
 # model.add(Activation('sigmoid'))
 model.add(Dense(1))
-model.compile(loss='mean_squared_error', optimizer=SGD(lr=0.02, momentum=0., decay=0., nesterov=False))
+model.compile(loss='mean_squared_error', optimizer=SGD(lr=0.1, momentum=0., decay=0., nesterov=False))
 
 model.fit(
     [tr[:,0].reshape((L,1)), tr[:,1].reshape((L,1))],
     tr[:,2].reshape((L,1)),
-    batch_size=32,
-    nb_epoch=20,
+    batch_size=8,
+    nb_epoch=40,
     validation_data=(
         [ts[:,0].reshape((M,1)), ts[:,1].reshape((M,1))],
         ts[:,2].reshape((M,1))
     )
 )
-
-result = model.predict([ts[:,0].reshape((M,1)), ts[:,1].reshape((M,1))])
-
-err = 0
-for i in range(M):
-    err += (result[i][0] - ts[i][2]) ** 2
-err = math.sqrt(err/M)
-print("%.6f"%err)
-
 
